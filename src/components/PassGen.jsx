ef3dbf6757460f4399acdb79af1bc948cffc8c14
bilal <bilalsuri9898@gmail.com>
2025-03-18 04:20:56 +0500

    add functionalities

--- src/components/PassGen.jsx
+++ src/components/PassGen.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState,useCallback } from 'react'
 import {Container, InputCheck, InputRange} from './index'
 
 const PassGen = () => {
@@ -7,9 +7,41 @@ const PassGen = () => {
     const [characters,setCharacters] = useState(false)
     const [uppercase,setUppercase] = useState(false)
     const [lowercase,setLowercase] = useState(false)
+    const [password,setPassword] = useState('')
+
+    const passwordGenerator = useCallback(() => {
+        let pass = ''
+        let string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+        if (number) string += '0123456789'
+        if (characters) string += '!@#$%^&*()'
+        if (uppercase) string += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        if (lowercase) string += 'abcdefghijklmnopqrstuvwxyz'
+
+        for (let i = 1; i <= passLength; i++) {
+            const char = Math.floor((Math.random()) * (string.length + 1))
+            pass += string.at(char)
+        }
+        setPassword(pass)
+      },[passLength,number,characters,uppercase,lowercase,setPassword])
+    
+      useEffect(()=>{
+        passwordGenerator()
+      },[passLength,number,characters,uppercase,lowercase,passwordGenerator])
 
     function handleRange(event) {
         setPassLength(event.target.value)
+    }
+    function handleNumber() {
+        setNumber((prev)=>!prev)
+    }
+    function handleCharacter() {
+        setCharacters((prev)=>!prev)
+    }
+    function handleUpperCase() {
+        setUppercase((prev)=>!prev)
+    }
+    function handleLowerCase() {
+        setLowercase((prev)=>!prev)
     }
   return (
     <Container>
@@ -18,19 +50,43 @@ const PassGen = () => {
                 <input 
                 type="text" 
                 placeholder='password' 
-                className='w-[80%] p-2 border-none outline-none rounded-xl'/>
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
+                className='w-[80%] cursor-default p-2 border-none outline-none rounded-xl'
+                readOnly
+                />
                 <button 
-                className='px-4 py-2 rounded-xl cursor-pointer capitalize text-white bg-slate-500'>
+                className='px-4 py-2 font-bold rounded-xl cursor-pointer capitalize text-white bg-slate-500'>
                     copy
                 </button>
             </div>
             <div className="bottom inline-flex gap-2 flex-wrap  mt-5 mb-2 justify-center">
                 <InputRange Label={'length'} range={passLength} onRange={handleRange}/>
 
-                <InputCheck Label={"number"} Id={"number"}/>
-                <InputCheck Label={"characters"} Id={"characters"}/>
-                <InputCheck Label={"uppercase"} Id={"uppercase"}/>
-                <InputCheck Label={"lowercase"} Id={"lowercase"}/>
+                <InputCheck 
+                Label={"number"}
+                 Id={"number"}
+                 isChecked={number}
+                 checkInput={handleNumber}
+                 />
+                <InputCheck 
+                Label={"characters"}
+                 Id={"characters"}
+                 isChecked={characters}
+                 checkInput={handleCharacter}
+                 />
+                <InputCheck 
+                Label={"uppercase"}
+                 Id={"uppercase"}
+                 isChecked={uppercase}
+                 checkInput={handleUpperCase}
+                 />
+                <InputCheck 
+                Label={"lowercase"}
+                 Id={"lowercase"}
+                 isChecked={lowercase}
+                 checkInput={handleLowerCase}
+                 />
             </div>
         </div>
     </Container>
