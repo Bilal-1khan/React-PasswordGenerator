d827a688534fc3e71ea6adcb6ae842dd3ec7a197
bilal <bilalsuri9898@gmail.com>
2025-03-18 03:31:02 +0500

    add index.js

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Container from './container/Container'
+import PassGen from './PassGen'
+import InputRange from "./Input_Range/InputRange"
+import InputCheck from './Input_Check/InputCheck'
+
+export{
+    Container,PassGen,InputRange,InputCheck
+}
