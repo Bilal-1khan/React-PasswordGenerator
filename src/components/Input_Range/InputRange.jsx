5db819fb539b4cee053b56d5a1329128e11dd30d
bilal <bilalsuri9898@gmail.com>
2025-03-18 03:36:20 +0500

    add change

---
+++ src/components/Input_Range/InputRange.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const InputRange = ({Label,range,onRange}) => {
+  
+  return (
+    <>
+        <input 
+        type="range" 
+        min={5}
+        max={50}
+        id="inputRange" 
+        className='mx-3'
+        value={range}
+        onChange={(event)=>onRange(event)}
+        />
+        <label 
+        htmlFor="inputRange"
+           className='text-white capitalize cursor-pointer'
+        >{Label} ({range})</label>
+    </>
+  )
+}
+
+export default InputRange
