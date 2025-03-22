9f8e37251dc3e4d198efe5ca4a8459739f703313
bilal <bilalsuri9898@gmail.com>
2025-03-22 12:46:32 +0500

    update file

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import PassGen from './PassGen'
 import InputRange from "./Input_Range/InputRange"
 import InputCheck from './Input_Check/InputCheck'
 import InputPass from './Input_Pass/InputPass'
+import Button from './Button/Button'
 
 export{
     Container,
     PassGen,
     InputRange,
     InputCheck,
-    InputPass
+    InputPass,
+    Button
 }
